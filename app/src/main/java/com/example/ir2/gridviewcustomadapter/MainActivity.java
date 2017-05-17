77a16379e9d157c4e4d202cf092664c10f211321
ir2 <usir2@hotmail.com>
2017-05-18 00:32:46 +0700

    Edit text

--- app/src/main/java/com/example/ir2/gridviewcustomadapter/MainActivity.java
+++ app/src/main/java/com/example/ir2/gridviewcustomadapter/MainActivity.java
@@ -35,38 +35,38 @@ public class MainActivity extends AppCompatActivity {
 
     private void addItemToArray(){
         // Add items to ArrayList by Item class
-        // 3 parameters 1.head 2.body 3.end
+        // 3 parameters 1.HEAD 2.body 3.end
         itemlist.add(new Item("HEAD 1",R.mipmap.ic_launcher,"END 1"));
-        itemlist.add(new Item("head 2",R.mipmap.ic_launcher,"END 2"));
-        itemlist.add(new Item("head 3",R.mipmap.ic_launcher,"END 3"));
-        itemlist.add(new Item("head 4",R.mipmap.ic_launcher,"END 4"));
-        itemlist.add(new Item("head 5",R.mipmap.ic_launcher,"END 5"));
-        itemlist.add(new Item("head 6",R.mipmap.ic_launcher,"END 6"));
-        itemlist.add(new Item("head 7",R.mipmap.ic_launcher,"END 7"));
-        itemlist.add(new Item("head 8",R.mipmap.ic_launcher,"END 8"));
-        itemlist.add(new Item("head 9",R.mipmap.ic_launcher,"END 9"));
-        itemlist.add(new Item("head 10",R.mipmap.ic_launcher,"END 10"));
-        itemlist.add(new Item("head 11",R.mipmap.ic_launcher,"END 11"));
-        itemlist.add(new Item("head 12",R.mipmap.ic_launcher,"END 12"));
-        itemlist.add(new Item("head 13",R.mipmap.ic_launcher,"END 13"));
-        itemlist.add(new Item("head 14",R.mipmap.ic_launcher,"END 14"));
-        itemlist.add(new Item("head 15",R.mipmap.ic_launcher,"END 15"));
-        itemlist.add(new Item("head 16",R.mipmap.ic_launcher,"END 16"));
-        itemlist.add(new Item("head 17",R.mipmap.ic_launcher,"END 17"));
-        itemlist.add(new Item("head 18",R.mipmap.ic_launcher,"END 18"));
-        itemlist.add(new Item("head 19",R.mipmap.ic_launcher,"END 19"));
-        itemlist.add(new Item("head 20",R.mipmap.ic_launcher,"END 20"));
-        itemlist.add(new Item("head 21",R.mipmap.ic_launcher,"END 21"));
-        itemlist.add(new Item("head 22",R.mipmap.ic_launcher,"END 22"));
-        itemlist.add(new Item("head 23",R.mipmap.ic_launcher,"END 23"));
-        itemlist.add(new Item("head 24",R.mipmap.ic_launcher,"END 24"));
+        itemlist.add(new Item("HEAD 2",R.mipmap.ic_launcher,"END 2"));
+        itemlist.add(new Item("HEAD 3",R.mipmap.ic_launcher,"END 3"));
+        itemlist.add(new Item("HEAD 4",R.mipmap.ic_launcher,"END 4"));
+        itemlist.add(new Item("HEAD 5",R.mipmap.ic_launcher,"END 5"));
+        itemlist.add(new Item("HEAD 6",R.mipmap.ic_launcher,"END 6"));
+        itemlist.add(new Item("HEAD 7",R.mipmap.ic_launcher,"END 7"));
+        itemlist.add(new Item("HEAD 8",R.mipmap.ic_launcher,"END 8"));
+        itemlist.add(new Item("HEAD 9",R.mipmap.ic_launcher,"END 9"));
+        itemlist.add(new Item("HEAD 10",R.mipmap.ic_launcher,"END 10"));
+        itemlist.add(new Item("HEAD 11",R.mipmap.ic_launcher,"END 11"));
+        itemlist.add(new Item("HEAD 12",R.mipmap.ic_launcher,"END 12"));
+        itemlist.add(new Item("HEAD 13",R.mipmap.ic_launcher,"END 13"));
+        itemlist.add(new Item("HEAD 14",R.mipmap.ic_launcher,"END 14"));
+        itemlist.add(new Item("HEAD 15",R.mipmap.ic_launcher,"END 15"));
+        itemlist.add(new Item("HEAD 16",R.mipmap.ic_launcher,"END 16"));
+        itemlist.add(new Item("HEAD 17",R.mipmap.ic_launcher,"END 17"));
+        itemlist.add(new Item("HEAD 18",R.mipmap.ic_launcher,"END 18"));
+        itemlist.add(new Item("HEAD 19",R.mipmap.ic_launcher,"END 19"));
+        itemlist.add(new Item("HEAD 20",R.mipmap.ic_launcher,"END 20"));
+        itemlist.add(new Item("HEAD 21",R.mipmap.ic_launcher,"END 21"));
+        itemlist.add(new Item("HEAD 22",R.mipmap.ic_launcher,"END 22"));
+        itemlist.add(new Item("HEAD 23",R.mipmap.ic_launcher,"END 23"));
+        itemlist.add(new Item("HEAD 24",R.mipmap.ic_launcher,"END 24"));
     }
 
 
 
     private void addItemToArrayByLoop(){
         // Add 24 item to ArrayList use for Loop
-        // Item Class(3 parameters) ->  1.head 2.body 3.end
+        // Item Class(3 parameters) ->  1.HEAD 2.body 3.end
         for(int i=0;i<24;i++) {
             itemlist.add(new Item("HEAD : "+i, R.mipmap.ic_launcher, "END : "+i));
         }
