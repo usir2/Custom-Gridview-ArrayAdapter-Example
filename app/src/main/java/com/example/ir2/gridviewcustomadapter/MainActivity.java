2349f4bd12d25376719e3bce1426769daf4fa3ac
ir2 <usir2@hotmail.com>
2017-05-17 19:42:37 +0700

    init project

---
+++ app/src/main/java/com/example/ir2/gridviewcustomadapter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ir2.gridviewcustomadapter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
